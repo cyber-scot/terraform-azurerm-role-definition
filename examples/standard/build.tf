c41572ce0bc7b7a26b2f0d582bf99a3fc1740620
Craig Thacker <craigthackerx@gmail.com>
2023-09-28 23:01:52 +0100

    Update module

--- examples/standard/build.tf
+++ examples/standard/build.tf
@@ -1,5 +1,39 @@
-module "test" {
+module "rg" {
+  source = "cyber-scot/rg/azurerm"
+
+  name     = "rg-${var.short}-${var.loc}-${var.env}-01"
+  location = local.location
+  tags     = local.tags
+}
+
+resource "azurerm_user_assigned_identity" "uaid" {
+  resource_group_name = module.rg.rg_name
+  location            = module.rg.rg_location
+  tags                = module.rg.rg_tags
+
+  name = "id-${var.short}-${var.loc}-${var.env}-build"
+}
+
+module "custom_role_definition" {
   source = "../../"
 
-
+  roles = [
+    {
+      create_role      = true
+      assign           = true
+      name             = "CustomRole1"
+      description      = "This is a custom role 1"
+      definition_scope = format("/subscriptions/%s", data.azurerm_client_config.current.subscription_id)
+      principal_id     = azurerm_user_assigned_identity.uaid.principal_id
+      permissions = [
+        {
+          actions          = ["Microsoft.Authorization/*/read"]
+          not_actions      = []
+          data_actions     = []
+          not_data_actions = []
+        }
+      ]
+      assignment_scope = module.rg.rg_id
+    },
+  ]
 }
--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
-#
+resource "azurerm_role_definition" "custom" {
+  for_each = { for role in var.roles : role.name => role if role.create_role }
+
+  name        = each.value.name
+  description = each.value.description
+  scope       = each.value.definition_scope
+
+  dynamic "permissions" {
+    for_each = each.value.permissions
+    content {
+      actions          = lookup(permissions.value, "actions", [])
+      not_actions      = lookup(permissions.value, "not_actions", [])
+      data_actions     = lookup(permissions.value, "data_actions", [])
+      not_data_actions = lookup(permissions.value, "not_data_actions", [])
+    }
+  }
+}
+
+resource "azurerm_role_assignment" "custom" {
+  for_each = { for role in var.roles : role.name => role if lookup(role, "assign", true) }
+
+  scope              = each.value.assignment_scope
+  role_definition_name = azurerm_role_definition.custom[each.key].name
+  principal_id       = each.value.principal_id
+}
